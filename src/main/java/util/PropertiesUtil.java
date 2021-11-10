468c5ebf67fd56ce1c11ad141920c401df2904c3
“周虎” <zhouhu@leyoujia.com>
2021-11-10 10:35:13 +0800

    自定义输入数据源

---
+++ src/main/java/util/PropertiesUtil.java
@@ -0,0 +1,57 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+/**
+ * @author lenovo
+ */
+public class PropertiesUtil {
+        public static Properties confProperties;
+        static {
+            try {
+                init();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void init() throws IOException {
+            if (confProperties == null) {
+                confProperties = new Properties();
+                InputStream in = PropertiesUtil.class.getClassLoader().getResourceAsStream("config.properties");
+
+                try {
+                    confProperties.load(in);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(null != in){
+                        in.close();
+                    }
+
+                }
+            }
+        }
+
+        public static Properties getProperties() throws IOException {
+            init();
+            return confProperties;
+        }
+
+        public static void clear() {
+            confProperties.clear();
+            confProperties = null;
+        }
+
+        public static String get(String key) {
+            try {
+                init();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return confProperties.getProperty(key);
+        }
+}
