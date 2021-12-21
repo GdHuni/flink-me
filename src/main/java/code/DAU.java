a998ebb87add1d43cab1ae81ce96aa0fcdf94a27
“周虎” <zhouhu@leyoujia.com>
2021-12-21 15:26:59 +0800

    sink to ch 修改

--- src/main/java/code/DAU.java
+++ src/main/java/code/DAU.java
@@ -11,12 +11,17 @@ import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.evictors.TimeEvictor;
@@ -27,6 +32,8 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 
 import java.net.URLDecoder;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
@@ -34,7 +41,7 @@ import java.util.Properties;
 
 /**
  * @Classname DAU
- * @Description 使用flink计算 日活，这个案列是计算报表平台 的针对各个报表的日活
+ * @Description 使用flink计算 日活，这个案列是计算报表平台 的针对各个报表的日活输出到clickhouse
  *  * 1.flume 监控 /home/bigdata/logs/new_click_all_view.log.2021-12-02文件 输出到kafka
  *  * 2.从kafka中消费数据
  *  * 3.过滤掉只拿自助详情页链接
@@ -103,7 +110,7 @@ public class DAU {
                     }
                 });
         KeyedStream<Tuple2<PcWapVo, Integer>, String> KeyedStream = flatMap.keyBy(x -> x.f0.getType()+"_"+x.f0.getL_date()+"_"+x.f0.getReportId());
-        KeyedStream.window(TumblingEventTimeWindows.of(Time.days(1),Time.hours(-8)))
+        SingleOutputStreamOperator<PcWapVo> process = KeyedStream.window(TumblingEventTimeWindows.of(Time.days(1), Time.hours(-8)))
                 .trigger(ContinuousProcessingTimeTrigger.of(Time.seconds(100)))
                 .evictor(TimeEvictor.of(Time.seconds(0), true))
                 .process(new ProcessWindowFunction<Tuple2<PcWapVo, Integer>, PcWapVo, String, TimeWindow>() {
@@ -112,7 +119,7 @@ public class DAU {
                             加evictor 是因为，每次trigger，触发计算是，窗口中的所有数据都会参与，所以数据会触发很多次，比较浪费，加evictor 驱逐已经计算过的数据，就不会重复计算了
                             驱逐了已经计算过的数据，导致窗口数据不完全，所以需要state 存储我们需要的中间结果*/
                     ValueState<Tuple2<String, Long>> pvCount;
-                    MapState<String,String> uvCount;
+                    MapState<String, String> uvCount;
 
                     @Override
                     public void open(Configuration parameters) throws Exception {
@@ -124,7 +131,9 @@ public class DAU {
                         );
                         MapStateDescriptor wordStateDes = new MapStateDescriptor<>(
                                 "uvCount",
-                                TypeInformation.of(new TypeHint<String>() {}),TypeInformation.of(new TypeHint<String>() {})
+                                TypeInformation.of(new TypeHint<String>() {
+                                }), TypeInformation.of(new TypeHint<String>() {
+                        })
                         );
 
 
@@ -138,17 +147,17 @@ public class DAU {
                     public void process(String s, Context context, Iterable<Tuple2<PcWapVo, Integer>> elements, Collector<PcWapVo> out) throws Exception {
                         long pv = 0;
                         for (Tuple2<PcWapVo, Integer> element : elements) {
-                            pv ++;
+                            pv++;
                             String word = element.f0.getWid();
                             uvCount.put(word, null);
                         }
-                        if(null == pvCount.value()){
-                            pvCount.update(new Tuple2<>(s,pv));
-                        }else{
-                            pvCount.update(new Tuple2<>(s,pvCount.value().f1 + pv));
+                        if (null == pvCount.value()) {
+                            pvCount.update(new Tuple2<>(s, pv));
+                        } else {
+                            pvCount.update(new Tuple2<>(s, pvCount.value().f1 + pv));
                         }
 
-                        long  count  = 0;
+                        long count = 0;
                         Iterator<String> iterator = uvCount.keys().iterator();
                         while (iterator.hasNext()) {
                             iterator.next();
@@ -160,13 +169,48 @@ public class DAU {
                         vo.setUv(count);
                         out.collect(vo);
                     }
-                }).print();
-                /** 这里有个问题就是如果是插入到ch的话，但ch是不支持update和delete的（不友好），然后吧 引擎 设置为 ENGINE = ReplacingMergeTree
+                });
+        /** 这里有个问题就是如果是插入到ch的话，但ch是不支持update和delete的（不友好），然后吧 引擎 设置为 ENGINE = ReplacingMergeTree
                  但是他又是不定时更新的。所以一直不能实时起来，后面想的是  吧数据实时落地到ch中，不做处理，在建一个视图来进行count 和count（distinct id）来统计pv,uv
 
                  */
 
+        //sink,采用JdbcSink工具类 (flink 1.11 版本提供)
+        /**
+         * sink方法需要依次传入四个参数：1.预编译的SQL字符串
+         * 2.实现JdbcStatementBuilder的对象，用于对预编译的SQL进行传值
+         *  3.JDBC执行器，可以设置批量写入等参数，负责具体执行写入操作
+         * 4.JDBC连接设置 包含连接驱动、URL、用户名、密码
+         * 说明：不同的数据库在jdbc连接设置部分传入不同的驱动、url等参数即可
+         */
+        String sql = "insert into tmp.jdbc_example (loc,pv,date) values (?,?,?)";
+        SinkFunction<PcWapVo> sink = JdbcSink.sink(sql, new MysqlBuilder(),
+                JdbcExecutionOptions.builder().withBatchSize(50).build(),
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                        .withDriverName("ru.yandex.clickhouse.ClickHouseDriver")
+                        .withUrl("jdbc:clickhouse://172.16.5.32:8123/tmp")
+                        .withUsername("default")
+                        //.withPassword("root")
+                        .build());
+
+        process.addSink(sink);
+
         env.execute();
 
+    }
+
+    //自定义StatementBuilder 实现accept方法实现对预编译的SQL进行传值
+    public static class MysqlBuilder implements JdbcStatementBuilder<PcWapVo> {
+
+        @Override
+        public void accept(PreparedStatement pst, PcWapVo vo)  {
+            try {
+                pst.setString(1,vo.getLoc());
+                pst.setLong(2,vo.getPv());
+                pst.setString(3, vo.getL_date());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 }
